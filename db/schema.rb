3885212d8bbe367ffdcc6a56e0e1e11455cfe9d3
mrjln <marjolein_tromp@hotmail.com>
2017-11-13 19:56:33 +0100

    generate Student Model and declare associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113184913) do
+ActiveRecord::Schema.define(version: 20171113185314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20171113184913) do
     t.integer "number"
     t.date "starts_at"
     t.date "ends_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "student_id"
+    t.index ["student_id"], name: "index_batches_on_student_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -40,4 +48,5 @@ ActiveRecord::Schema.define(version: 20171113184913) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "batches", "students"
 end
