e687195ad34bd6b166c4d86f9c28d729b4508e7e
mrjln <marjolein_tromp@hotmail.com>
2017-11-14 15:02:26 +0100

    test for picking a semi random color on green

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -15,6 +15,13 @@ class Batch < ApplicationRecord
   def students_evaluation(string)
     @filtered_array_of_students = []
 
+  end
+
+  def self.pick_semi_random_color
+#50% chance to pick red, 33% chance to pick yellow, 17% chance to pick green
+    chances_colors = ["red","red","red","yellow","yellow","green"]
+    color_selector = chances_colors.sample
+    return color_selector
 
   end
 end
--- spec/models/batch_spec.rb
+++ spec/models/batch_spec.rb
@@ -3,3 +3,10 @@ require 'rails_helper'
 #RSpec.describe Batch, type: :model do
 #  pending "add some examples to (or delete) #{__FILE__}"
 #end
+
+describe ".pick_semi_random_color" do
+
+    it "returns color" do
+      expect(Batch.pick_semi_random_color).to eq("red").or(eq("yellow")).or(eq("green"))
+    end
+  end
