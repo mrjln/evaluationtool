327832f165e487e82bde252dd5ffc88b28c363d8
mrjln <marjolein_tromp@hotmail.com>
2017-11-14 21:38:50 +0100

    styling

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,16 +2,6 @@ class Batch < ApplicationRecord
   has_many :students
 
 
-
-  def create_array_of_students
-    @array_of_students = []
-    self.students.each do |student|
-      @array_of_students << student.name
-    end
-
-    return @array_of_students
-  end
-
   def student_to_ask_question
     color_selector = self.pick_semi_random_color
     students_grouped = self.students_grouped_by_color(color_selector)
@@ -29,7 +19,7 @@ class Batch < ApplicationRecord
   end
 
   def pick_semi_random_color
-    #50% chance to pick red, 33% chance to pick yellow, 17% chance to pick green
+    #50% chance to pick red = 4x , 33% chance to pick yellow = 2x, 17% chance to pick green = 1x
     chances_colors = ["red","red","red","yellow","yellow","green"]
     color_selector = chances_colors.sample
     return color_selector
