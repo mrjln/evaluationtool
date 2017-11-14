4e2805f88e3455ca9b175b8d19adba225e177bab
mrjln <marjolein_tromp@hotmail.com>
2017-11-14 16:47:04 +0100

    created tests

--- spec/models/batch_spec.rb
+++ spec/models/batch_spec.rb
@@ -5,8 +5,23 @@ require 'rails_helper'
 #end
 
 describe ".pick_semi_random_color" do
+  let(:batch) {create :batch}
 
     it "returns color" do
-      expect(Batch.pick_semi_random_color).to eq("red").or(eq("yellow")).or(eq("green"))
+      expect(batch.pick_semi_random_color).to eq("red").or(eq("yellow")).or(eq("green"))
     end
   end
+
+  describe ".return_percentage" do
+    let(:batch) {create :batch}
+    let(:student1) { create :student, batch: batch }
+
+    let(:evaluation) { create :evaluation, color: "red", student: student1 }
+    let(:evaluation) { create :evaluation, color: "red", student: student1 }
+    let(:evaluation) { create :evaluation, color: "yellow", student: student1 }
+    let(:evaluation) { create :evaluation, color: "green", student: student1 }
+
+      it "returns percentage of students with the color red, yellow or blue" do
+        expect(batch.return_percentage("red")).to eq(50)
+      end
+    end
