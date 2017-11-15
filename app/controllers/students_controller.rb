d049286165a687c799e01f6dfa5bc9a6ef6c5be2
mrjln <marjolein_tromp@hotmail.com>
2017-11-15 13:39:14 +0100

    create student action compleet

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,37 @@
 class StudentsController < ApplicationController
+  before_action :set_batch
 
   def show
     @student = Student.find(params[:id])
-    @batch = @student.batch
   end
 
   def new
-    @student= Student.new
+     @student = @batch.students.build
   end
+
+  def create
+      @student = @batch.students.build(student_params)
+
+      if @student.save
+          redirect_to batch_path(@batch), notice: 'Student added'
+      else
+        redirect_to batch_path(@batch), notice: 'Student was not added'
+      end
+  end
+
+
+  private
+
+  def set_batch
+   @batch = Batch.find(params[:batch_id])
+ end
+
+
+  def student_params
+   params
+     .require(:student)
+     .permit(:name, evaluations:[])
+
+   end
+
 end
