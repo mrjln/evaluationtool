f17238053d303552e3a4eaecf53117be4d34039d
mrjln <marjolein_tromp@hotmail.com>
2017-11-17 00:46:56 +0100

    styling for selected student

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   version :thumbnail do
     eager
-    resize_to_fit(200, 200)
+    resize_to_fit(150, 150)
     cloudinary_transformation :quality => 80
   end
 end
