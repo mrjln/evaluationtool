adc3b8f1bb769de8496d5680f439211397e2ab4b
mrjln <marjolein_tromp@hotmail.com>
2017-11-17 11:07:12 +0100

    generate photos controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
 
-
+  resources :photos, only: [:destroy]
 
   resources :batches, except: [:destroy] do
     resources :students do
