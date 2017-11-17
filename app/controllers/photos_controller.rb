6a4abdc85eb15d0d86788823751e5e54ad8bf19f
mrjln <marjolein_tromp@hotmail.com>
2017-11-17 11:11:24 +0100

    remove photo of user

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,13 @@
 class PhotosController < ApplicationController
+before_action :authenticate_user!
+
+  def destroy
+      photo = Photo.find(params[:id])
+      @student = photo.student
+      @batch = @student.batch
+      photo.destroy
+      redirect_to edit_batch_student_path(@batch, @student), notice: "Photo removed"
+    end
+
+
 end
